125cbfad4c47d55ff4d539ff40a7215981d267c6
hyosunko <hyosunko@gmail.com>
2019-06-28 14:08:11 -0700

    add onclick and submit events

--- app.js
+++ app.js
@@ -1,5 +1,86 @@
-ReactDOM.render(React.createElement(
-  "h1",
-  null,
-  "Hello, world"
-), document.getElementById("root"));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Todo = function (_React$Component) {
+  _inherits(Todo, _React$Component);
+
+  function Todo(props) {
+    _classCallCheck(this, Todo);
+
+    var _this = _possibleConstructorReturn(this, (Todo.__proto__ || Object.getPrototypeOf(Todo)).call(this, props));
+
+    _this.state = {
+      done: _this.props.done == "true" && props.done,
+      text: props.text
+    };
+
+    _this.handleClick = _this.handleClick.bind(_this);
+    _this.handleChange = _this.handleChange.bind(_this);
+    _this.handleSubmit = _this.handleSubmit.bind(_this);
+    return _this;
+  }
+
+  _createClass(Todo, [{
+    key: "handleClick",
+    value: function handleClick(event) {
+      var _this2 = this;
+
+      this.setState(function (state) {
+        return {
+          done: !_this2.state.done
+        };
+      }, function (event) {
+        this.handleSubmit(event);
+      });
+    }
+  }, {
+    key: "handleChange",
+    value: function handleChange(event) {
+      var text = event.target.value;
+      this.setState(function (state) {
+        return {
+          text: text
+        };
+      });
+    }
+  }, {
+    key: "handleSubmit",
+    value: function handleSubmit(event) {
+      console.log("This is where submit will happen");
+      // this.setState(state => ({
+      // }))
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "todo" },
+        React.createElement(
+          "span",
+          null,
+          React.createElement("input", {
+            type: "checkbox",
+            checked: this.state.done,
+            onClick: this.handleClick
+          }),
+          React.createElement("input", {
+            type: "text",
+            value: this.state.text,
+            onChange: this.handleChange,
+            onBlur: this.handleSubmit
+          })
+        )
+      );
+    }
+  }]);
+
+  return Todo;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Todo, { text: "Todo 1", done: "true" }), document.getElementById("root"));
--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,63 @@
-ReactDOM.render(<h1>Hello, world</h1>, document.getElementById("root"));
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: this.props.done == "true" && props.done,
+      text: props.text
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState(
+      state => ({
+        done: !this.state.done
+      }),
+      function(event) {
+        this.handleSubmit(event);
+      }
+    );
+  }
+
+  handleChange(event) {
+    let text = event.target.value;
+    this.setState(state => ({
+      text: text
+    }));
+  }
+
+  handleSubmit(event) {
+    console.log("This is where submit will happen");
+    // this.setState(state => ({
+    // }))
+  }
+
+  render() {
+    return (
+      <div className="todo">
+        <span>
+          <input
+            type="checkbox"
+            checked={this.state.done}
+            onClick={this.handleClick}
+          />
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+            onBlur={this.handleSubmit}
+          />
+        </span>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Todo text="Todo 1" done="true" />,
+  document.getElementById("root")
+);
